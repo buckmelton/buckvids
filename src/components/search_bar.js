005f20329e4f7ff5a5b2d2d94f80886eacdedfb6
Buck <buck9063@gmail.com>
2017-11-07 00:08:11 -0800

    Convert SearchBar into controlled component

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -3,11 +3,17 @@ import React from 'react';
 class SearchBar extends React.Component {
   constructor(props) {
     super(props);
-    this.state={term: ''};
+    this.state={ term: '' };
   }
 
   render() {
-    return <input onChange={event => console.log(event.target.value)}/>;
+    return (
+      <div>
+        <input
+          value={this.state.term}
+          onChange={event => this.setState({ term: event.target.value })}/>
+      </div>
+    );
   }
 }
 
