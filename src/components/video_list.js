6a749261190d0b23dfced22ecc710ad6f771583b
Buck <buck9063@gmail.com>
2017-11-08 00:37:35 -0800

    Add VideoListItem component

--- src/components/video_list.js
+++ src/components/video_list.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import VideoListItem from './video_list_item';
 
 const VideoList = (props) => {
+
+  const videoItems = props.videos.map((video) => {
+    return <VideoListItem video={video} />
+  });
+
   return (
     <ul className="col-md-4 list-grouo">
-      {props.videos.length}
+      {videoItems}
     </ul>
   );
 }
