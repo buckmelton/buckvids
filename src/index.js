789e0ee15d843d834c15654f67192b8cf54928c3
Buck Melton <bmelton@mdsol.com>
2017-11-09 12:53:30 -0800

    Add selected video to state and first video as initial selected video

--- src/index.js
+++ src/index.js
@@ -11,9 +11,15 @@ const API_KEY = 'AIzaSyBncyh4uNmieuX_Xdw1vQ7MXYSdISdmxts';
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = { videos: [] };
+    this.state = {
+      videos: [],
+      selectedVideo: null
+    };
     YTSearch({ key: API_KEY, term: 'surfboards' }, (videos) => {
-      this.setState({ videos });
+      this.setState({
+        videos: videos,
+        selectedVideo: videos[0]
+      });
     });
   }
 
@@ -21,7 +27,7 @@ class App extends Component {
     return (
       <div>
         <SearchBar />
-        <VideoDetail video={this.state.videos[0]} />
+        <VideoDetail video={this.state.selectedVideo} />
         <VideoList videos={this.state.videos} />
       </div>
     );
